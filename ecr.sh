560471469831b1b483f9e97563295839550e8bd9
Iam-Tech02 <davearvind002@gmail.com>
2025-04-09 17:53:21 +0530

    updated ECR

--- ecr.sh
+++ ecr.sh
@@ -11,6 +11,7 @@ if [ -z "$IMAGE_TAG" ]; then
 fi
 
 aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
+
 docker build -t "$REPOSITORY_NAME:$IMAGE_TAG" .
 docker tag "$REPOSITORY_NAME:$IMAGE_TAG" "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPOSITORY_NAME:$IMAGE_TAG"
 docker push "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$REPOSITORY_NAME:$IMAGE_TAG"
